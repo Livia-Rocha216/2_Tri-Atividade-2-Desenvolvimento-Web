09a1c2372a13e2d5365c5b0934eadcb77c5419ee
Lívia Rocha <liviaantenucci@colegiopaulodetarso.com.br>
2025-05-19 16:19:49 +0000

    Finalizado

---
+++ calculadora.js
@@ -0,0 +1,18 @@
+function somar(a,b){
+    return a + b
+}
+
+function subtrair(a,b){
+    return a - b
+}
+function dividir(a,b){
+    return a / b
+}
+function multiplicar(a,b){
+    return a * b
+}
+
+console.log("Soma:", somar(1,2))
+console.log("Subtração:", subtrair(1,2))
+console.log("Divisão:", dividir(1,2))
+console.log("Multiplicação:", multiplicar(1,2))
---
+++ comparacao.js
@@ -0,0 +1,5 @@
+function ehMaior(x,y){}
+
+console.log(ehMaior(10,5)); //true
+console.log(ehMaior(2,8)); // false
+console.log(ehMaior(7,7)); // false
